21bf9a8f923c7f05cee41e9e1aed3b18cb48f28d
Hoanghung2000 <hoanghung0902000@gmail.com>
2024-03-28 22:59:58 +0700

    code ex1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,24 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
-        System.out.println();
+        int num = 0;
+        boolean flag = true;
+        Scanner scanner = new Scanner(System.in);
+        num = scanner.nextInt();
+        for (int i = 2; i < num/2; i++) {
+            flag = false;
+            break;
+        }
+
+        if (flag == true){
+            System.out.println("đây là số nguyên tố!");
+        }
+        else{
+            System.out.println("đây không là số nguyên tố!");
+        }
+
     }
 }
