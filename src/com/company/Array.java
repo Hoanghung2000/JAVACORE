1bf00b88ad4fba0387cfcd5f42cda88a3fd5a84c
Hoanghung2000 <hoanghung0902000@gmail.com>
2024-03-31 01:40:23 +0700

    section3

--- src/com/company/Array.java
+++ src/com/company/Array.java
@@ -10,7 +10,7 @@ public class Array {
         //null
 
         // cach 2
-        String[] names = new String[]{
+        String[] name = new String[]{
                 "hung",
                 "hoang",
                 "tran"
@@ -24,10 +24,42 @@ public class Array {
             System.out.println(ages[i]);
         }*/
 
-        for (int age: ages
+        /*for (int age: ages
         ) {
             System.out.println(age);
+        }*/
+
+        //multidimensional array
+        //cach 1
+        int[][] matrixAge = new int[2][3];
+        matrixAge[0][0] = 1;
+        matrixAge[0][1] = 12;
+        matrixAge[0][2] = 4;
+
+        matrixAge[1][0] = 2;
+        matrixAge[1][1] = 7;
+        matrixAge[1][2] = 6;
+
+        for (int i = 0; i < matrixAge.length ; i++) {
+            for (int j = 0; j < matrixAge[i].length; j++) {
+                System.out.printf(matrixAge[i][j] + " ");
+            }
+            System.out.println(" ");
         }
+
+        //cach 2
+        String[][] names = new  String[][]{
+                {"Linh", "lee", "nam"},
+                {"Tu", "Ngoc", "Duong"}
+        };
+
+        for (int i = 0; i < names.length; i++) {
+            for (int j = 0; j < names[i].length; j++) {
+                System.out.printf(names[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+
     }
 }
 
